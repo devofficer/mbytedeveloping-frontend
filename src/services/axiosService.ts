2b68a3d7f301fa6e265bf6b1541dd28dd2caaed3
devofficer <nikolaytoplev@gmail.com>
2023-01-03 17:55:15 -0500

    fix: setup api proxy for axios

--- src/services/axiosService.ts
+++ src/services/axiosService.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosResponse } from 'axios';
+import { PROXY_URL } from '../config';
 
-const axiosService = axios.create();
+const axiosService = axios.create({
+  baseURL: PROXY_URL,
+});
 
 axiosService.interceptors.response.use((res: AxiosResponse<any, any>) => {
   return res.data;
